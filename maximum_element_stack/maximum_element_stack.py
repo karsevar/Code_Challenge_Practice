3191f581a81e6e28986cae0d4e5d198d4702db6b
masonkarsevar <masonkarsevar@gmail.com>
2020-06-04 13:43:17 -0700

    fixed a small bug code passes all tests

--- maximum_element_stack/maximum_element_stack.py
+++ maximum_element_stack/maximum_element_stack.py
@@ -1,44 +1,48 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-import sys
-
-# first create a singly linked list class this will be the base data structure 
-# used for this problem (the stack)
-
-# first create a node class that will only have a marker to the next node 
 class Node:
-    def __init__(self, val):
+    def __init__(self, val, current_max):
         self.val = val
         self.prev = None
+        self.current_max = current_max
 
 class SingleLinkedList:
     def __init__(self):
         self.head = None
-        self.largest_val = None
+        self.largest_val = 0
 
     def insert(self, val):
+        # print('largest value', self.largest_val)
+        
         if self.head == None:
-            self.head = Node(val)
+            self.head = Node(val, val)
             self.largest_val = val
+            # print('largest value node', self.head.current_max)
+            # print('node value', self.head.val)
         else:
+            # print('largest value node', self.head.current_max)
+            # print('node value', val)
 
             if self.largest_val < val:
-                new_head = Node((2*val) - self.largest_val) 
+                new_head = Node(val, val) 
                 new_head.prev = self.head 
                 self.head = new_head
                 self.largest_val = val
             else:
-                new_head = Node(val) 
+                new_head = Node(val, self.largest_val) 
                 new_head.prev = self.head 
                 self.head = new_head
             
 
     def delete_head(self):
         if self.head:
+            if self.head.val == self.largest_val:
+                if self.head.prev:
+                    self.largest_val = self.head.prev.current_max
+                else:
+                    self.largest_val = self.head.val
             new_head = self.head.prev
             self.head = new_head
 
-            if new_head != None and new_head.val > self.largest_val:
-                self.largest_val = (2*self.largest_val) - new_head.val
+            
 
             # if self.head != None and self.head.val == self.largest_val[-1]:
             #     self.largest_val.pop()
@@ -52,25 +56,13 @@ class SingleLinkedList:
 
 input_data = sys.stdin.readlines()
 cleaned_data = []
-for data in input_data:
-    new_data = data.rstrip().split()
-    cleaned_data.append(new_data)
-
-# create a command flow 
-# loop through the cleaned_data array (ignoring of course the first index) 
-    # if the first index in cleaned_data[i] is 1 
-        # insert the second index value into the SingleLinkedList 
-    # elif the first index in cleaned_data[i] is 2 
-        # delete the head node of the singlelinked list
-    # elif the first index in cleaned_data[i] is 3
-        # iterate through the singlelinkedlist and find the largest value in the 
-        # list 
-
 stack = SingleLinkedList()
-for command in cleaned_data[1:]:
+for data in input_data[1:]:
+    command = data.rstrip().split()
+    # cleaned_data.append(new_data)
     if command[0] == '1':
         stack.insert(int(command[1]))
     elif command[0] == '2':
         stack.delete_head()
     elif command[0] == '3':
-        print(stack.largest_val)
+        print(stack.head.current_max)
