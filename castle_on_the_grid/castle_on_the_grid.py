ff4c90a8f7dad589909fd7791ebe169941419a50
masonkarsevar <masonkarsevar@gmail.com>
2020-06-12 09:59:39 -0700

    passed almost all test cases except for one due to a timeout condition. WIP: find a more optimal solution

--- castle_on_the_grid/castle_on_the_grid.py
+++ castle_on_the_grid/castle_on_the_grid.py
@@ -1,3 +1,13 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import deque 
+
+# Complete the minimumMoves function below.
 def minimumMoves(grid, startX, startY, goalX, goalY):
     # print(grid)
     # print('start', startX, startY)
@@ -25,128 +35,65 @@ def minimumMoves(grid, startX, startY, goalX, goalY):
         # grid or equal to x 
             # add the node x and y coordinates to the neighbors array
 
-        if (indexX + 1) < len(grid[0]): 
-            if grid[indexX+1][indexY] != 'X':
-                neighbors.append((indexX+1, indexY, 'R'))
-
-        if (indexX - 1) >= 0:
-            if grid[indexX-1][indexY] != 'X':
-                neighbors.append((indexX-1, indexY, 'L'))
-
-        if (indexY-1) >= 0:
-            if grid[indexX][indexY-1] != 'X':
-                neighbors.append((indexX, indexY-1, 'U'))
-
-        if (indexY + 1) < len(grid):
-            if grid[indexX][indexY+1] != 'X':
-                neighbors.append((indexX, indexY+1, 'D'))
-    
-        return neighbors
-    
-    print(get_neighbors(startX, startY, grid))
-
-    # create the breadth first traversal algorithm:
-    # first create a queue 
-    # create a visited set 
-
-    # initialize the queue with the starting node coordinates in 
-    # (x, y, direction) format, Make sure to structure the queue elements 
-    # as an array inside an array example [[(x, y, direction), ...]] to keep 
-    # track of traversal path 
-
-    # create a while loop that will terminate once the queue is empty 
-    #   pop the direction from the beginning of the queue 
-    #   check if the last item in the popped path is not in visited 
-    #       if so add the node to the visited set 
-    #       add the neighbors to the queue 
-
-    queue = []
-    queue.append([(startX, startY, 'S')])
-    result_path = []
-
-    visited = set() 
-
-    while len(queue) > 0:
-        path = queue.pop(0)
-
-        if path[-1] not in visited:
-
-            if path[-1][0] == goalX and path[-1][1] == goalY:
-                result_path = list(path)
+        x_axis = indexX + 1
+        while x_axis < len(grid[0]):
+            if grid[x_axis][indexY] != 'X':
+                neighbors.append((x_axis, indexY))
+                x_axis += 1
+            else:
                 break
 
-            for neighbor in get_neighbors(path[-1][0], path[-1][1], grid):
-                new_path = list(path)
-                new_path.append(neighbor) 
-                queue.append(new_path)
+        x_axis = indexX - 1
+        while x_axis >= 0:
+            if grid[x_axis][indexY] != 'X':
+               neighbors.append((x_axis, indexY))
+               x_axis -= 1 
+            else:
+                break
 
-    moves = 0
-    past_move = 'S'
+        y_axis = indexY - 1
+        while y_axis >= 0:
+            if grid[indexX][y_axis] != 'X':
+                neighbors.append((indexX, y_axis))
+                y_axis -= 1
+            else:
+                break
+        
+        y_axis = indexY + 1
+        # print('indexY', indexY)
+        
+        while y_axis < len(grid):
+            # print('y_axis after up movement 2', grid[indexX][y_axis])
+            if grid[indexX][y_axis] != 'X':
+                neighbors.append((indexX, y_axis))
+                y_axis += 1
+            else:
+                break 
 
-    for direction in result_path:
-        if past_move != direction[2]:
-            moves += 1
-            past_move = direction[2]
+        # find the coordinate with the highest or lowest X value 
+        highest_xcoordinate = neighbors[0]
+        lowest_xcoordinate = neighbors[0]
+        highest_ycoordinate = neighbors[0]
+        lowest_ycoordinate = neighbors[0]
+        
+        for x_coordinate in neighbors[1:]:
+            if x_coordinate[0] > highest_xcoordinate[0]:
+                highest_xcoordinate = x_coordinate 
+            if x_coordinate[0] < lowest_xcoordinate[0]:
+                lowest_xcoordinate = x_coordinate
 
-    return moves
+            if x_coordinate[1] > highest_ycoordinate[1]:
+                highest_ycoordinate = x_coordinate 
 
+            if x_coordinate[1] < lowest_ycoordinate[1]:
+                lowest_ycoordinate = x_coordinate 
+        
+        # print('neighbors full array', neighbors)
 
-## Refactor attempt:
-def minimumMoves(grid, startX, startY, goalX, goalY):
-    # print(grid)
-    # print('start', startX, startY)
-    # print('goal', goalX, goalY)
+        # print('neighbors print', lowest_ycoordinate, highest_ycoordinate, lowest_xcoordinate, highest_xcoordinate)
 
-    # create a get neighbors function that will be able to check 
-    # if the nodes to the right, left, top, and bottom of the current 
-    # node exists or is an x.
+        # return [highest_xcoordinate, lowest_xcoordinate, highest_ycoordinate, lowest_ycoordinate]
 
-    def get_neighbors(indexX, indexY, direction, moves, grid):
-        # neighbors array will take in the coordinates as (x, y)
-        neighbors = []
-        # if X value to the right of indexX is not the end of the passed in string 
-        # or is not x 
-            # add the node x and y coordinates to the neighbors array
-
-        # if x value to the left of indexX is not less than the index 0 or 
-        # is not x 
-            # add the node x and y coordinates to the neighbors array 
-
-        # if y value on top of indexY is not equal to -1 or x 
-            # add the node x and y corrdinates to the neighbors array
-
-        # if y value on the bottom of indexY is not more than the Y axis of the 
-        # grid or equal to x 
-            # add the node x and y coordinates to the neighbors array
-
-        if (indexX + 1) < len(grid[0]): 
-            if grid[indexX+1][indexY] != 'X':
-                if direction == 'R':
-                    neighbors.append((indexX+1, indexY, 'R', moves))
-                else:
-                    neighbors.append((indexX+1, indexY, 'R', moves+1))
-
-        if (indexX - 1) >= 0:
-            if grid[indexX-1][indexY] != 'X':
-                if direction == 'L':
-                    neighbors.append((indexX-1, indexY, 'L', moves))
-                else:
-                    neighbors.append((indexX-1, indexY, 'L', moves+1))
-
-        if (indexY-1) >= 0:
-            if grid[indexX][indexY-1] != 'X':
-                if direction == 'U':
-                    neighbors.append((indexX, indexY-1, 'U', moves))
-                else:
-                    neighbors.append((indexX, indexY-1, 'U', moves+1))
-
-        if (indexY + 1) < len(grid):
-            if grid[indexX][indexY+1] != 'X':
-                if direction == 'D':
-                    neighbors.append((indexX, indexY+1, 'D', moves))
-                else:
-                    neighbors.append((indexX, indexY+1, 'D', moves))
-    
         return neighbors
     
     # print(get_neighbors(startX, startY, grid))
@@ -167,31 +114,28 @@ def minimumMoves(grid, startX, startY, goalX, goalY):
     #       add the neighbors to the queue 
 
     queue = []
-    queue.append((startX, startY, 'S', 0))
+    queue.append([(startX, startY)])
     result_path = []
 
     visited = set() 
 
     while len(queue) > 0:
         path = queue.pop(0)
+        print(path)
 
-        if (path[0], path[1]) not in visited:
-            visited.add((path[0], path[1]))
+        if path[-1] not in visited:
+            visited.add(path[-1])
 
-            if path[0] == goalX and path[1] == goalY:
-                return path[3] + 1
+            if path[-1][0] == goalX and path[-1][1] == goalY:
+                result_path= path
                 
-            for neighbor in get_neighbors(path[0], path[1], path[2], path[3], grid):
-                # new_path = list(path)
-                # new_path.append(neighbor) 
-                queue.append(neighbor)
 
-    # moves = 0
-    # past_move = 'S'
+            for neighbor in get_neighbors(path[-1][0], path[-1][1], grid):
+                if neighbor not in visited:
+                    new_path = list(path)
+                    new_path.append(neighbor) 
+                    queue.append(new_path)
 
-    # for direction in result_path:
-    #     if past_move != direction[2]:
-    #         moves += 1
-    #         past_move = direction[2]
+    print(result_path)
 
-    # return moves
+    return len(result_path) - 1
