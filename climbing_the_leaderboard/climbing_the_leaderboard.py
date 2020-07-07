8f13252a379cf31e4a19a99c9fa505253cb9c71b
masonkarsevar <masonkarsevar@gmail.com>
2020-07-07 12:00:46 -0700

    optimized the code with a binary search. solution is passing all test cases

--- climbing_the_leaderboard/climbing_the_leaderboard.py
+++ climbing_the_leaderboard/climbing_the_leaderboard.py
@@ -29,3 +29,48 @@ def climbingLeaderboard(scores, alice):
                 else:
                     rankings.append(score_index + 2)
     return rankings
+
+def climbingLeaderboardRevised(scores, alice):
+
+    # create a new array that deletes all the repeating scores 
+    new_scores = []
+    for index in range(1, len(scores)):
+        if len(new_scores) == 0:
+            new_scores.append(scores[index-1])
+        if scores[index-1] > scores[index]:
+            new_scores.append(scores[index])
+
+    rankings = []
+    
+    if len(scores) > 1:
+        for alice_score in alice:
+            rankings.append(binary_search(0, len(new_scores)-1, new_scores, alice_score) + 1)
+    else:
+        for alice_score in alice:
+            if scores[0] > alice_score:
+                rankings.append(2)
+            elif scores[0] <= alice_score:
+                rankings.append(1)
+
+    return rankings
+
+def binary_search(start, end, scores, alice_score):
+    # print('alice score', alice_score)
+    while end >= start:
+        # print('start', start)
+        # print('end', end)
+        middle = (start + end) // 2 
+        # print('middle', middle, 'middle value', scores[middle], '\n')
+
+        if start == end:
+            if scores[start] > alice_score:
+                return start + 1
+            else:
+                return start
+
+        if scores[middle] > alice_score:
+            start = middle + 1 
+        elif scores[middle] < alice_score:
+            end = middle
+        elif scores[middle] == alice_score:
+            return middle
