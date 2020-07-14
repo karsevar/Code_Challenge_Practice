acc3522897e297bd4aefa07dd5cf57a94d4b45e0
masonkarsevar <masonkarsevar@gmail.com>
2020-07-14 11:11:33 -0700

    code challenge solution. needed help from a tutorial

--- save_the_prisoner/save_the_prisoner.py
+++ save_the_prisoner/save_the_prisoner.py
@@ -23,3 +23,26 @@ def saveThePrisoner(n, m, s):
 
         if s > n:
             s = 1 
+
+def saveThePrisonerRefactored(n, m, s):
+    # print(n)
+    # print(m) 
+    # print(s)
+
+    # the main idea is to increment s by one and start again 
+    # to the first chair once the s variable is equal to n (
+    # much like a ring buffer.
+
+    # in addition make sure to increment m by one with each current 
+    # chair position 
+
+    # the naive solution is to create a while loop that will terminate 
+    # once m is equal to zero (since m is the number of snacks to 
+    # distribute).
+
+    rest = (s+m-1) % n
+
+    if rest == 0:
+        return n 
+    else:
+        return rest
