5dce0feb9178faf6fe001097e401bdba8eabe550
masonkarsevar <masonkarsevar@gmail.com>
2020-11-16 15:10:50 -0800

    almost finished append and delete challenge. Need to optimize code to pass two test cases

---
+++ append_and_delete/append_and_delete.py
@@ -0,0 +1,40 @@
+def appendAndDelete(s, t, k):
+    # print('array s', s[6:])
+    # print('array t', t[6:])
+    
+    # create a variable that will hold where the descrepency begins 
+    # create a for loop that will loop through both of the strings 
+        # if at current index current letter in s is different than current
+        # letter of t or len(s) - 1 is equal to index or len(t) - 1 is equal to 
+        # index
+            # return descrepency index 
+        
+    divergence = None 
+    
+    for index in range(len(s)):
+        if len(s) - 1 == index and len(t) - 1 != index:
+            divergence = index + 1
+            break
+        elif len(s) - 1 != index and len(t) - 1 == index:
+            divergence = index + 1
+            break
+        elif len(s) - 1 == index and len(t) - 1 == index:
+            divergence = -1
+            break
+        elif s[index] is not t[index]:
+            divergence = index 
+            break 
+        
+    if divergence == -1:
+        return 'Yes'
+    else:
+        print('divergence value', divergence)
+                
+        diffS = len(s[divergence:])
+        diffT = len(t[divergence:])
+        print('sum of divergence', diffS + diffT)
+        
+        if diffS + diffT > k:
+            return 'No'
+        else:
+            return 'Yes'
