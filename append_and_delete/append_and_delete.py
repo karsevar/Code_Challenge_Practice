d7f53faff010349728220d73d09c7b53365fc49b
masonkarsevar <masonkarsevar@gmail.com>
2020-11-18 10:26:05 -0800

    unable to solve this problem on my own. Still had trouble with test cases 10 and 3

--- append_and_delete/append_and_delete.py
+++ append_and_delete/append_and_delete.py
@@ -38,3 +38,21 @@ def appendAndDelete(s, t, k):
             return 'No'
         else:
             return 'Yes'
+
+def appendAndDeleteSolution(s, t, k):
+    s_length = len(s)
+    t_length = len(t)
+
+    if s_length + t_length < k: return 'Yes'
+
+    same = 0
+    for s_l, t_l in zip(s, t):
+        if s_l == t_l: same += 1
+        else: break
+   
+    extra_s = s_length - same
+    extra_t = t_length - same
+    difference = extra_s + extra_t
+
+    if difference <= k and difference % 2 == k % 2: return 'Yes'
+    return 'No'
